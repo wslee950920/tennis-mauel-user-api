2071ac1cdb8755a47f55f00bc05b3517bdfb1850
wslee950920 <wslee950920@gmail.com>
2021-11-12 14:31:39 +0900

    initail commit

---
+++ src/main/java/com/tennismauel/user/health/HealthCheckController.java
@@ -0,0 +1,10 @@
+package com.tennismauel.user.health;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/@user/health")
+@RestController
+public class HealthCheckController {
+
+}
